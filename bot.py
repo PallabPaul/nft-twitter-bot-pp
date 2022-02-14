d309d62a53260fbb27f07e0e2b8eb9eb6e14b7ed
PallabPaul <pallab.paul000@gmail.com>
2022-02-13 22:32:37 -0500

    added commenting feature

--- bot.py
+++ bot.py
@@ -1,57 +1,113 @@
 import time
 import sys
 import tweepy
-# from dotenv import load_dotenv
 from random import randint
-from list_of_tweets import status_arr, hashtag_arr, comment_arr
-
-# load_dotenv()
-
-# from credentials import *  # use this one for testing
-
-# use this for production; set vars in heroku dashboard
 from os import environ
-CONSUMER_KEY = environ['CONSUMER_KEY']
-CONSUMER_SECRET = environ['CONSUMER_SECRET']
-ACCESS_KEY = environ['ACCESS_KEY']
-ACCESS_SECRET = environ['ACCESS_SECRET']
+from list_of_tweets import status_arr, hashtag_arr, comment_arr
+from dotenv import load_dotenv
+load_dotenv()
 
-INTERVAL = 60 * 60 * 1  # tweet every 6 hours
-# INTERVAL = 15  # every 15 seconds, for testing
+class TwitterBot:
 
-auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
-auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
-api = tweepy.API(auth)
+    def __init__(self):
 
-def generate_status(prev_status_index):
+        CONSUMER_KEY = environ['CONSUMER_KEY']
+        CONSUMER_SECRET = environ['CONSUMER_SECRET']
+        ACCESS_KEY = environ['ACCESS_KEY']
+        ACCESS_SECRET = environ['ACCESS_SECRET']
 
-    status_index = randint(0,len(status_arr)-1)
+        auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+        auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
 
-    while prev_status_index == status_index:
-        status_index = randint(0,len(status_arr)-1)
+        self.api = tweepy.API(auth)
+
+        self.INTERVAL = 60 * 30
+
+        self.prev_status_index = -1
+
+        self.status_count = 1
+        self.comment_count = 1
+
+        self.last_post_id = 0
+
+    def generate_status(self,is_comment=False):
+
+        sentence_arr = comment_arr if is_comment else status_arr
+
+        status_index = randint(0,len(sentence_arr)-1)
+
+        while self.prev_status_index == status_index:
+            status_index = randint(0,len(sentence_arr)-1)
+        
+        status = sentence_arr[status_index]+" "+hashtag_arr[randint(0,len(hashtag_arr)-1)]
+        return status if len(status) <= 140 else self.generate_status(is_comment)
+
+    def get_random_media_file(self):
+
+        png_filename = "./images/{}.png".format(randint(1,19))
+        gif_filename = "./images/1.gif"
+        return gif_filename if randint(1,5) == 5 else png_filename
+
+    def send_tweet(self,is_comment=False,id=None,name=None):
+
+        status = self.generate_status(is_comment)
     
-    status = status_arr[status_index]+" "+hashtag_arr[randint(0,len(hashtag_arr)-1)]
-    return status,status_index if len(status) <= 140 else generate_status(prev_status_index)
+        if is_comment:
+            status = "@"+name+" "+status
 
-def get_random_media_file():
-    png_filename = "./images/{}.png".format(randint(1,19))
-    gif_filename = "./images/1.gif"
-    return gif_filename if randint(1,5) == 5 else png_filename
+        media_or_not = randint(1,4)
+        if media_or_not == 4 and not is_comment:
+            self.api.update_status(status, in_reply_to_status_id=id)
+        else:
+            self.api.update_status_with_media(status, self.get_random_media_file(), in_reply_to_status_id=id)
+        
 
-# results = api.search_tweets(q="NFT")
+    def tweet_a_status(self):
 
-# for result in results:
-#     print (result.text)
-count = 1
-status_index = -1
-while True:
-    print("updating status # {}".format(count))
-    status,status_index = generate_status(status_index)
-    print(status)
-    media_or_not = randint(1,4)
-    if media_or_not == 4:
-        api.update_status(status)
-    else:
-        api.update_status_with_media(status, get_random_media_file())
-    count += 1
-    time.sleep(INTERVAL)
+        print("Updating status # {}".format(self.status_count))
+        self.send_tweet()
+        self.status_count += 1
+
+    def query_tweets(self):
+        
+        results = self.api.search_tweets(
+            q="(NFT AND DROP AND YOUR) OR (NFT AND DROP AND YOUR AND BUYING) -ETH",
+            count=15,
+            since_id=self.last_post_id)
+
+        id_name_dict = {}
+
+        for result in results:
+            if (result.text[:2]) == "RT":
+                result = result.retweeted_status
+            id_name_dict[result.id] = result.user.screen_name
+
+        if id_name_dict:
+            self.last_post_id = max(id_name_dict.keys())
+        
+        return id_name_dict
+
+    def tweet_comments(self):
+
+        tweet_dict = self.query_tweets()
+        if tweet_dict:
+            for id, name in tweet_dict.items():
+                print("Commenting on tweet # {}".format(self.comment_count))
+                self.send_tweet(True,id,name)
+                self.comment_count += 1
+                time.sleep(90)
+
+    def start_bot(self):
+
+        while True:
+            self.tweet_a_status()
+            time.sleep(self.INTERVAL)
+            self.tweet_comments()
+            time.sleep(self.INTERVAL)
+            self.tweet_comments()
+            time.sleep(self.INTERVAL)
+
+if __name__ == '__main__':
+     bot = TwitterBot()
+     print("Starting bot...")
+     bot.start_bot()
--- list_of_tweets.py
+++ list_of_tweets.py
@@ -5,14 +5,25 @@ status_arr = [
     "PauliePunks is one of the only NFT projects to donate all earnings to charity!",
     "Retweet, like and follow to be entered in our giveaway of a 1/1 PauliePunk! pauliepunks.com",
     "Drop your best NFTs and let's build an NFT community. pauliepunks.com",
-    "Follow and stay tuned for details about our mint. All earnings go to St. Jude's!"
+    "Follow and stay tuned for details about our mint. All earnings go to St. Jude's!",
+    "Be sure to follow and check out pauliepunks.com for updates on the mint date!",
+    "Follow, retweet and comment your ETH address for a PauliePunks airdrop giveaway!"
 ]
 
 hashtag_arr = [
     "#NFT #NFTdrop #nftcollector #CharityHour",
     "#NFT #NFTs #ETH #Crypto #charity #NFTCollection",
     "#NFT #NFTs #NFTCommunity #NFTartist #NFTdrop",
-    "#NFT #MintOfTheDay #OpenSeaNFT #NFTs #NFTLAUNCH"
+    "#NFT #MintOfTheDay #OpenSeaNFT #NFTs #NFTLAUNCH",
+    "#NFTLAUNCH #NFTdrop #ETH #PauliePunks #Crypto"
 ]
 
-comment_arr = []
+comment_arr = [
+    "Check out pauliepunks.com all profits going to charity!",
+    "PauliePunks mint drop will be soon, be sure to follow for updates!",
+    "Follow for updates PauliePunks NFTs will be dropping soon!",
+    "PauliePunks NFT mint drop coming soon, all proceeds being donated to St. Jude's!",
+    "Check out pauliepunks.com",
+    "Unique 1/1 NFTs - PauliePunks! Check them out!!!",
+    "pauliepunks.com check it out. Mint drop soon for 0.01 ETH each!"
+]
